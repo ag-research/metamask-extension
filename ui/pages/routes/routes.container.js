51f80d0959609c9dadc3758b6373984a59204057
NidhiKJha <menidhikjha@gmail.com>
2023-09-07 15:55:18 +0530

    show on wallet and connections view

--- ui/pages/routes/routes.container.js
+++ ui/pages/routes/routes.container.js
@@ -12,7 +12,6 @@ import {
   getCurrentChainId,
   getShouldShowSeedPhraseReminder,
   isCurrentProviderCustom,
-  getUnapprovedTransactions,
 } from '../../selectors';
 import {
   lockMetamask,
@@ -60,7 +59,6 @@ function mapStateToProps(state) {
     isNetworkUsed: getIsNetworkUsed(state),
     allAccountsOnNetworkAreEmpty: getAllAccountsOnNetworkAreEmpty(state),
     isTestNet: getIsTestnet(state),
-    unapprovedTransactions: getUnapprovedTransactions(state),
     currentChainId: getCurrentChainId(state),
     shouldShowSeedPhraseReminder: getShouldShowSeedPhraseReminder(state),
     forgottenPassword: state.metamask.forgottenPassword,
